f5ffc2fb023efc6d87ad67f441744065253c9145
Jure Cuhalev <gandalfar@gmail.com>
2016-01-27 21:10:37 +0100

    Started work on home page

--- app/application/controller.js
+++ app/application/controller.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   session: Ember.inject.service('session'),
-
+  isHomepage: Ember.computed.equal('currentRouteName', 'index'),
   actions: {
     logout() {
       this.get('session').invalidate();
