ceae8527946c6b9c4c38f23c5f9dec5b050b43fc
Jure Cuhalev <gandalfar@gmail.com>
2016-01-30 11:03:29 +0100

    fix twitter feed

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,5 @@
 import DRFAdapter from './drf';
-import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
+// import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 // export default DRFAdapter.extend(DataAdapterMixin, {
 //   authorizer: 'authorizer:token',
