2e95e5aebc18e9c2f9bf3ed4ae5b6a1927da86ce
Jure Cuhalev <gandalfar@gmail.com>
2016-02-03 16:12:13 +0100

    Event routes

--- app/events/event/route.js
+++ app/events/event/route.js
@@ -1,4 +1,12 @@
-import Ember from 'ember';
-
 export default Ember.Route.extend({
+  model(params) {
+    return this.store.query('event', { slug: params.event_slug })
+    .then(function(pages) {
+      return pages.get('firstObject');
+    });
+  },
+
+  titleToken: function(model) {
+    return model.get('title');
+  },
 });
