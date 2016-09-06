7f42a1365d23b1ca49c3e55f9ca2e44ac616241a
Jure Cuhalev <gandalfar@gmail.com>
2016-09-06 16:42:08 +0200

    Support resubmitting content

--- app/submit/route.js
+++ app/submit/route.js
@@ -3,12 +3,18 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.createRecord('submission', {
-      // 'datetime': 'Mon Mar 27 2017 15:00:00 GMT+0200 (CEST)'
-      'datetime': new Date(2017, 2, 27)
+      'datetime': new Date(2017, 2, 27) // also update date in controller.resetSubmission();
     });
   },
 
+  setupController: function(controller, model) {
+    this._super(controller, model);
+    controller.resetData(controller);
+  },
+
   resetController(controller, isExiting) {
+    this._super(controller, isExiting);
+
     if (isExiting) {
       var model = controller.get('model');
       if (model.get('isNew')) {
