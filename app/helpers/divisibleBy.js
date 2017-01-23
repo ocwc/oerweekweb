a1e319c203800a576837a046886e03d82a9b46d4
Jure Cuhalev <jure@cuhalev.com>
2017-01-23 15:05:24 +0100

    Removed unused openphotos

--- app/helpers/divisibleBy.js
+++ app/helpers/divisibleBy.js
@@ -4,7 +4,7 @@ export function divisibleBy(params/*, hash*/) {
   let index = params[0] + 1,
       modulo = params[1];
 
-  if ( index % modulo === 0) {
+  if (index % modulo === 0) {
     return true;
   }
   return false;
